83df5d370ead54bb20a9f9a7cc6765d5fecfbb63
khrisyu9 <khrisyu@umich.edu>
2024-10-17 01:56:02 -0400

    added speed test to benchmark

--- benchmark_llr.R
+++ benchmark_llr.R
@@ -1,21 +1,37 @@
 source("llr_functions.R")
-# Try out functions to see if it works on some data:
 library(reshape2) # package that contains "french_fries"
+library(microbenchmark) # for benchmarking
+
+# Try out functions to see if it works on some data:
 data(french_fries)
 french_fries = french_fries[complete.cases(french_fries),]
 z = seq(0, 15, length.out = 100)
-fits = llr(z = z, x = french_fries$potato, y = french_fries$buttery, omega = 2)
-plot(z, fits)
 
-# Loop through different omega values
-for (i in seq(1,5,0.5)){
-  fits = llr(z = z, x = french_fries$potato, y = french_fries$buttery, omega = i)
+# Benchmarking the llr function with omega = 2
+benchmark_result <- microbenchmark(
+  fits = llr(z = z, x = french_fries$potato, y = french_fries$buttery, omega = 2),
+  times = 10  # Run the benchmark multiple times
+)
+
+# Print out the timing result
+cat("Benchmark results for llr function with omega = 2:\n")
+print(benchmark_result)
+
+# Loop through different omega values and benchmark them
+for (i in seq(1, 5, 0.5)) {
+  benchmark_result <- microbenchmark(
+    fits = llr(z = z, x = french_fries$potato, y = french_fries$buttery, omega = i),
+    times = 10
+  )
+  # Print out the timing result for each omega
+  cat("Benchmark results for llr function with omega =", i, ":\n")
+  print(benchmark_result)
+  
+  # Plot the fit
   plot(z, fits)
 }
 
-# We can see that the fit is getting more smoother when we increase the value of omega.
-# In locally weighted regression, the parameter omega controls how much of the 
-# surrounding data influences the fit at any given point. Larger omega increases the size of 
-# the window around the target point z, meaning more data points will contribute to 
-# the local regression fit for each point, thus effectively averaging out local fluctuations 
-# and reducing the impact of noise, leading to smoother curves.
+# We can see that the fit is getting smoother when we increase the value of omega.
+# In locally weighted regression, the parameter omega controls how much of the
+# surrounding data influences the fit at any given point.
+
